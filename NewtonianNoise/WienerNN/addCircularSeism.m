1b363ec3a5db3cef393f3189a04e4408197a49d7
Jan Harms <janosch314@gmail.com>
2015-04-06 22:24:05 +0200

    moved to Modern Controls

--- NewtonianNoise/WienerNN/addCircularSeism.m
+++
@@ -1,6 +0,0 @@
-function seism = addCircularSeism(seism,SN)
-
-phi = linspace(0,2*pi,SN+1)';
-phi(end) = [];
-
-seism.loc = [seism.loc; seism.L*[cos(phi) sin(phi)]];
--- NewtonianNoise/WienerNN/addSpiralSeism.m
+++
@@ -1,6 +0,0 @@
-function seism = addSpiralSeism(seism,A,SN)
-
-phi = linspace(0,2*pi*A,SN)';
-rho = linspace(0,seism.L,SN)';
-
-seism.loc = [cos(phi) sin(phi)].*rho(:,ones(1,2));
